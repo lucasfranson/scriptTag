896b821af1d10c31f7a8e597b5bfe56b023bbfdf
lucasfranson <37190481+lucasfranson@users.noreply.github.com>
2020-04-24 19:14:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-jQuery(document).ready(function() { $('body').prepend('<h1> Hello World from Lucas </h1>') }
+jQuery(document).ready(function() {	
+$('body').prepend('<div class="header" id="myHeader"><h1> Hello World from Lucas ' + data[0].author.login +' Modificando</h1></div>');
+		$('head').prepend('<style>.header { padding: 10px 16px; background: #555; color: #f1f1f1; } .content { padding: 16px; } .sticky { position: fixed; top: 0; width: 100%} .sticky + .content { padding-top: 102px; }</style>');
+
+		var header = document.getElementById("myHeader");
+		var sticky = header.offsetTop;
+
+		window.onscroll = function() { 
+			if (window.pageYOffset > sticky) {
+				header.classList.add("sticky");
+			} else {
+				header.classList.remove("sticky");
+			}
+		};
+  
+});
