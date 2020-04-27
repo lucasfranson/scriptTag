b368409aabc8deb07b0cfcf8220fc5013d688d8f
lucasfranson <37190481+lucasfranson@users.noreply.github.com>
2020-04-27 16:35:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function() {	
-		$('body').prepend('<div class="header" id="myHeader"><h1> Hello World from Lucas Modificando</h1></div>');
+		$('body').prepend('<div class="header" id="myHeader"><h1> Hello World from'+ {{ product.title }} + 'Lucas Modificando</h1></div>');
 		$('head').prepend('<style>.header { padding: 10px 16px; background: #555; color: #f1f1f1; } .content { padding: 16px; } .sticky { position: fixed; top: 0; width: 100%} .sticky + .content { padding-top: 102px; }</style>');
 
 		var header = document.getElementById("myHeader");
