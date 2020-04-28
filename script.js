eb1d2d2bbed1d2dfb6ff9e2d658cf89777c68927
lucasfranson <37190481+lucasfranson@users.noreply.github.com>
2020-04-28 09:58:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-jQuery(document).ready(function() {	
-		$('body').prepend('<div class="header" id="myHeader"><h1> Hello World from Lucas Modificando</h1></div>');
-		$('head').prepend('<style>.header { padding: 10px 16px; background: #555; color: #f1f1f1; } .content { padding: 16px; } .sticky { position: fixed; top: 0; width: 100%} .sticky + .content { padding-top: 102px; }</style>');
+jQuery(document).ready(function() {
+  let url = 'https://testezarbo.myshopify.com/admin/api/2020-04/orders.json';
+	let response = fetch(url)
+	.then((resp) => resp.json())
+	.then(function(data) {	
+    console.log(data);
+    $('body').prepend('<div class="header" id="myHeader"><h1> Hello World from Lucas Modificando</h1></div>');
+    $('head').prepend('<style>.header { padding: 10px 16px; background: #555; color: #f1f1f1; } .content { padding: 16px; } .sticky { position: fixed; top: 0; width: 100%} .sticky + .content { padding-top: 102px; }</style>');
 
-		var header = document.getElementById("myHeader");
-		var sticky = header.offsetTop;
+    var header = document.getElementById("myHeader");
+    var sticky = header.offsetTop;
 
-		window.onscroll = function() { 
-			if (window.pageYOffset > sticky) {
-				header.classList.add("sticky");
-			} else {
-				header.classList.remove("sticky");
-			}
-		};
-  
+    window.onscroll = function() { 
+      if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
+    };
+  });
+
 });
