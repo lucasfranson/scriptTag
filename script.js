c2cbd27aaf90da8fb7f6afcc11d773bfb9ab9908
lucasfranson <37190481+lucasfranson@users.noreply.github.com>
2020-04-29 14:27:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-jQuery(document).ready(function() {
-  let url = 'https://testezarbo.myshopify.com/api/2020-04/graphql.json';
-	let response = fetch(url, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify( '{ shop { name } }' ),
-  })
-	.then((resp) => resp.json())
-	.then(function(data) {	
-    console.log(data);
-    $('body').prepend('<div class="header" id="myHeader"><h1> Hello World from Lucas Modificando</h1></div>');
-    $('head').prepend('<style>.header { padding: 10px 16px; background: #555; color: #f1f1f1; } .content { padding: 16px; } .sticky { position: fixed; top: 0; width: 100%} .sticky + .content { padding-top: 102px; }</style>');
+ jQuery(document).ready(function() {
+    let url = 'https://testezarbo.myshopify.com/api/2020-04/graphql.json';
+    let response = fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json', 
+                  'X-Shopify-Storefront-Access-Token': '777174f29c084240dbede11722a7b9d9-1588166489' },
+      body: JSON.stringify( '{ shop { name } }' ),
+    })
+    .then((resp) => resp.json())
+    .then(function(data) {	
+      console.log(data);
+      $('body').prepend('<div class="header" id="myHeader"><h1> Hello World from Lucas Modificando</h1></div>');
+      $('head').prepend('<style>.header { padding: 10px 16px; background: #555; color: #f1f1f1; } .content { padding: 16px; } .sticky { position: fixed; top: 0; width: 100%} .sticky + .content { padding-top: 102px; }</style>');
 
-    var header = document.getElementById("myHeader");
-    var sticky = header.offsetTop;
+      var header = document.getElementById("myHeader");
+      var sticky = header.offsetTop;
 
-    window.onscroll = function() { 
-      if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-      } else {
-        header.classList.remove("sticky");
-      }
-    };
+      window.onscroll = function() { 
+        if (window.pageYOffset > sticky) {
+          header.classList.add("sticky");
+        } else {
+          header.classList.remove("sticky");
+        }
+      };
+    });
+
   });
-
-});
