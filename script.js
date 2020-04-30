858ff9fe6662c23247eb3a70e225f4b1184f10ba
lucasfranson <37190481+lucasfranson@users.noreply.github.com>
2020-04-30 17:07:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ jQuery(document).ready(function() {
       .then(function(data) {	
         console.log(data.total_price);
         var totalPrice = data.total_price/100;
-        $('body').prepend('<div class="header" id="myHeader"><h1> Hello World from Lucas Modificando</h1></div>');
+        $('body').prepend('<div class="header" id="myHeader"><h1> Hello World from Lucas' + totalPrice + 'Modificando</h1></div>');
         $('head').prepend('<style>.header { padding: 10px 16px; background: #555; color: #f1f1f1; } .content { padding: 16px; } .sticky { position: fixed; top: 0; width: 100%} .sticky + .content { padding-top: 102px; }</style>');
 
         var header = document.getElementById("myHeader");
